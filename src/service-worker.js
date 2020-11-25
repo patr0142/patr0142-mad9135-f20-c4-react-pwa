a6bf47402702f3040e6ee39b43ecf14021f9d7bd
an000029 <an000029@algonquinlive.com>
2020-11-25 11:13:02 -0500

    implemted caching images

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, CacheFirst} from 'workbox-strategies';
 
 clientsClaim();
 
@@ -50,13 +50,28 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
-    cacheName: 'images',
+  //({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  
+  ({ url }) => 
+  url.origin === self.location.origin && 
+  url.pathname.match(/\.(gif|png|jpg|jpeg)$/),
+  
+  // new StaleWhileRevalidate({
+  //   cacheName: 'images',
+  //   plugins: [
+  //     // Ensure that once this runtime cache reaches a maximum size the
+  //     // least-recently used images are removed.
+  //     new ExpirationPlugin({ maxEntries: 50 }),
+  //   ],
+  // })
+
+  new CacheFirst({
+    cacheName: 'images_in_CacheFirst',
     plugins: [
-      // Ensure that once this runtime cache reaches a maximum size the
-      // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 14 * 24 * 60 * 60, // 14 Days
+      }),
     ],
   })
 );
@@ -69,4 +84,13 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// Any other custom service worker logic can go here.
+
+self.addEventListener('fetch', (event) => {
+  console.log(`intercepting ${event.request.method} to ${event.request.url}`)
+})
+
+
+
+
+
+
